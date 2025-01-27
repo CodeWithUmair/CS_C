c2ae72267aac7324979534377d34bb0acc496147
CodeWithUmair <codewithumair867@gmail.com>
2025-01-28 04:46:00 +0500

    Square in p1

---
+++ practice/p1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Function to validate and get marks for a subject
+void Sqr() {
+    int num;
+    cout << "\n\n Enter Number : ";
+    cin >> num;
+    cout << "\n\n Square is : " << num * num;
+}
+
+int main() {
+   
+   Sqr();
+   return 0;
+}
