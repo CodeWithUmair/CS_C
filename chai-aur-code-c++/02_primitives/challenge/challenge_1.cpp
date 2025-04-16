9bcfc7de8c80db14ae47aed0e38163b3b7cbaff1
CodeWithUmair <codewithumair867@gmail.com>
2025-04-16 11:21:25 +0500

    challenge i solved

---
+++ chai-aur-code-c++/02_primitives/challenge/challenge_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string teaType = "Green Tea";
+    float pricePerKg = 1200.50;
+    char rating = 'A';
+
+    cout << "Tea Information:\n";
+    cout << "Type: \"" << teaType << "\"\n";
+    cout << "Price per kg: $" << pricePerKg << "\n";
+    cout << "Rating: " << rating << "\n";
+
+    return 0;
+}
